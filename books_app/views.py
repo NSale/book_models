2c91ddad8963aa1cd8739b223d5a61c6d37c9f09
NSale <gospasrb@yahoo.com>
2021-09-26 14:38:31 +0200

    add templates and routes

--- books_app/views.py
+++ books_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
+
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    return render(request, 'books_app/index.html', {
+        'books': books,
+    })
+
+
+def book_details(request, book_id):
+    # Depricated
+    # try:
+    #     book = Book.objects.get(id=book_id)
+    # except Exception as e:
+    #     raise Http404('There is no book with that index')
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'books_app/book_detail.html', {
+        'book': book,
+    })
