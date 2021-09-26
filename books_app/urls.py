b4df8093bc06245e3d5ce51dd2734105bdc74ff4
NSale <gospasrb@yahoo.com>
2021-09-26 23:35:45 +0200

    refactor code and add new infos

--- books_app/urls.py
+++ books_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('books', views.index, name='books'),
-    path('books/<int:book_id>', views.book_details, name='book_details')
+    path('books/<slug:slug>', views.book_details, name='book_details')
 ]
