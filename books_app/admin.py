102594d3905b1329ea029381a98c1551bc5adbd3
NSale <gospasrb@yahoo.com>
2021-09-27 22:56:42 +0200

    tweaking admin section

--- books_app/admin.py
+++ books_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Book
+
+
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ['slug', ] - need to remove this, because prepopulated_fields wouldn't work with readonly field
+    prepopulated_fields = {'slug': ('title', )}
+    list_filter = ('author', 'rating')
+    list_display = ('author', 'title', 'rating', 'is_bestselling')
+    search_fields = ('author', 'title')
+
+
+admin.site.register(Book, BookAdmin)
