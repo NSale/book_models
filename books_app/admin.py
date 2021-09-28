94b8a7ac975a8795ce30e0e076680e4cb4e55841
NSale <gospasrb@yahoo.com>
2021-09-29 00:27:21 +0200

    add Address and Country models and update the book_details view

--- books_app/admin.py
+++ books_app/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 
-from .models import Book, Author
+from .models import Book, Author, Address, Country
 
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ['slug', ] - need to remove this, because prepopulated_fields wouldn't work with readonly field
-    prepopulated_fields = {'slug': ('title', )}
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('author', 'rating')
-    list_display = ('author', 'title', 'rating', 'is_bestselling')
+    list_display = ('title', 'author', 'rating', 'is_bestselling')
     search_fields = ('author', 'title')
 
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'address')
+    list_filter = ('last_name',)
+
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
+    list_filter = ('name',)
+
 admin.site.register(Book, BookAdmin)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address)
+admin.site.register(Country, CountryAdmin)
